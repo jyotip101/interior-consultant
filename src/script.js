2364bbab2feb7863cc48d15da8952ada7aac480b
jyotip101 <jyotipatelfyit77100@gmail.com>
2021-11-11 18:39:44 +0530

    mobile menu

--- src/script.js
+++ src/script.js
@@ -1,10 +1,18 @@
 const navBar = document.querySelector('.nav-bar');
 const nav = document.querySelector('.mobile-nav');
-
+const mobileNav = document.querySelector('.mobile-links');
 let menuText; 
 nav.addEventListener('click', function () {
     
     menuText = navBar.textContent; 
      
-    navBar.textContent = menuText == 'menu' ? 'close' : 'menu';
+    // navBar.textContent = menuText == 'menu' ? 'close' : 'menu';
+
+    if(menuText == 'close') {
+        navBar.textContent = 'menu';
+        mobileNav.style.display = 'none';
+    } else {
+        navBar.textContent = 'close';
+        mobileNav.style.display = 'flex';
+    }
 });
